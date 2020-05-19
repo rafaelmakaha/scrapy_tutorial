c24f5e44d9b4ffde021e0c4ebc5a5023cec74a4f
Rafael Makaha <rafael.makaha@gmail.com>
2020-05-19 13:55:20 -0300

    Adds some options of calling links on a page.

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -4,7 +4,6 @@ class QuotesSpider (scrapy.Spider):
     name = "quotes"
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
     ]
 
     def parse(self, response):
@@ -14,7 +13,17 @@ class QuotesSpider (scrapy.Spider):
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall()
             }
-        next_page = response.css('li.next a::attr(href)').get()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+        # 1st Option
+        # for href in response.css('ul.pager a::attr(href)'):
+        #     yield response.follow(href, callback=self.parse)
+
+        # 2nd Option
+        # for a in response.css('ul.pager a'):
+        #     yield response.follow(a, callback=self.parse)
+
+        # 3rd Option
+        # anchors = response.css('ul.pager a')
+        # yield from response.follow_all(anchors, callback=self.parse)
+
+        # 4th Option
+        yield from response.follow_all(css='ul.pager a', callback=self.parse)
